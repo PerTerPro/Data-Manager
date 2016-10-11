f09e47b85deeab2c5a0c350bf808ad22dc8a48a6
xuantrangk54 <xuantrangk54@gmail.com>
2016-10-11 16:15:55 +0700

    ImageServer_CodeUp

---
+++ WSS.ImageServer/MssReport.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImboForm
+{
+    public class MssReport
+    {
+        public long ProductId { get; set; }
+        public string ImageId { get; set; }
+        public string PathImage { get; set; }
+        public string Error { get; set; }
+    }
+}
