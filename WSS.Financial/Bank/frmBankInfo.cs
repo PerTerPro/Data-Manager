6c96d85e138d65d7bdcb4e55f4f4e9384867c9b3
spider101091 <spider101091@gmail.com>
2016-10-12 08:45:47 +0700

    Up

---
+++ WSS.Financial/Bank/frmBankInfo.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSS.Financial.Bank
+{
+    public partial class FrmBankInfo : Form
+    {
+        private int _bankId;
+        public FrmBankInfo()
+        {
+            InitializeComponent();
+        }
+        public FrmBankInfo(int bankId)
+        {
+            InitializeComponent();
+            _bankId = bankId;
+        }
+        private void FrmBankInfo_Load(object sender, EventArgs e)
+        {
+            this.bankTableAdapter.Connection.ConnectionString = WssConnectionFinancial.ConnectionFinancial;
+            this.bankTableAdapter.FillBy_BankId(this.dBFinancial.Bank,_bankId);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Validate();
+                this.bankBindingSource.EndEdit();
+                this.bankTableAdapter.Update(this.dBFinancial.Bank);
+                MessageBox.Show(@"Okie!");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+            
+        }
+    }
+}
---
+++ WSS.Financial/Main.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace WSS.Financial
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.barManager1 = new DevExpress.XtraBars.BarManager();
+            this.bar2 = new DevExpress.XtraBars.Bar();
+            this.barSubItem1 = new DevExpress.XtraBars.BarSubItem();
+            this.barButtonItemBankBlending = new DevExpress.XtraBars.BarButtonItem();
+            this.barSubItemVayTinDung = new DevExpress.XtraBars.BarSubItem();
+            this.barButtonItemBankLending = new DevExpress.XtraBars.BarButtonItem();
+            this.barSubItem2 = new DevExpress.XtraBars.BarSubItem();
+            this.barButtonItemPaymentMethod = new DevExpress.XtraBars.BarButtonItem();
+            this.bar3 = new DevExpress.XtraBars.Bar();
+            this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
+            this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
+            this.dockManager1 = new DevExpress.XtraBars.Docking.DockManager();
+            this.dockPanel1 = new DevExpress.XtraBars.Docking.DockPanel();
+            this.dockPanel1_Container = new DevExpress.XtraBars.Docking.ControlContainer();
+            this.ctrBank1 = new WSS.Financial.Bank.ctrBank();
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).BeginInit();
+            this.dockPanel1.SuspendLayout();
+            this.dockPanel1_Container.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // barManager1
+            // 
+            this.barManager1.Bars.AddRange(new DevExpress.XtraBars.Bar[] {
+            this.bar2,
+            this.bar3});
+            this.barManager1.DockControls.Add(this.barDockControlTop);
+            this.barManager1.DockControls.Add(this.barDockControlBottom);
+            this.barManager1.DockControls.Add(this.barDockControlLeft);
+            this.barManager1.DockControls.Add(this.barDockControlRight);
+            this.barManager1.DockManager = this.dockManager1;
+            this.barManager1.Form = this;
+            this.barManager1.Items.AddRange(new DevExpress.XtraBars.BarItem[] {
+            this.barSubItemVayTinDung,
+            this.barButtonItemBankLending,
+            this.barSubItem1,
+            this.barButtonItemBankBlending,
+            this.barSubItem2,
+            this.barButtonItemPaymentMethod});
+            this.barManager1.MainMenu = this.bar2;
+            this.barManager1.MaxItemId = 6;
+            this.barManager1.StatusBar = this.bar3;
+            // 
+            // bar2
+            // 
+            this.bar2.BarName = "Main menu";
+            this.bar2.DockCol = 0;
+            this.bar2.DockRow = 0;
+            this.bar2.DockStyle = DevExpress.XtraBars.BarDockStyle.Top;
+            this.bar2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barSubItem1),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barSubItemVayTinDung),
+            new DevExpress.XtraBars.LinkPersistInfo(this.barSubItem2)});
+            this.bar2.OptionsBar.MultiLine = true;
+            this.bar2.OptionsBar.UseWholeRow = true;
+            this.bar2.Text = "Main menu";
+            // 
+            // barSubItem1
+            // 
+            this.barSubItem1.Caption = "Ngân hàng";
+            this.barSubItem1.Id = 2;
+            this.barSubItem1.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItemBankBlending)});
+            this.barSubItem1.Name = "barSubItem1";
+            // 
+            // barButtonItemBankBlending
+            // 
+            this.barButtonItemBankBlending.Caption = "Quản lý danh sách ngân hàng";
+            this.barButtonItemBankBlending.Id = 3;
+            this.barButtonItemBankBlending.Name = "barButtonItemBankBlending";
+            this.barButtonItemBankBlending.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItemBankBlending_ItemClick);
+            // 
+            // barSubItemVayTinDung
+            // 
+            this.barSubItemVayTinDung.Caption = "Vay tín dụng";
+            this.barSubItemVayTinDung.Id = 0;
+            this.barSubItemVayTinDung.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItemBankLending)});
+            this.barSubItemVayTinDung.Name = "barSubItemVayTinDung";
+            // 
+            // barButtonItemBankLending
+            // 
+            this.barButtonItemBankLending.Caption = "Quản lý các gói vay tín dụng";
+            this.barButtonItemBankLending.Id = 1;
+            this.barButtonItemBankLending.Name = "barButtonItemBankLending";
+            this.barButtonItemBankLending.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItemFrmBankLending_ItemClick);
+            // 
+            // barSubItem2
+            // 
+            this.barSubItem2.Caption = "Tool";
+            this.barSubItem2.Id = 4;
+            this.barSubItem2.LinksPersistInfo.AddRange(new DevExpress.XtraBars.LinkPersistInfo[] {
+            new DevExpress.XtraBars.LinkPersistInfo(this.barButtonItemPaymentMethod)});
+            this.barSubItem2.Name = "barSubItem2";
+            // 
+            // barButtonItemPaymentMethod
+            // 
+            this.barButtonItemPaymentMethod.Caption = "Hình thức trả khoản vay";
+            this.barButtonItemPaymentMethod.Id = 5;
+            this.barButtonItemPaymentMethod.Name = "barButtonItemPaymentMethod";
+            this.barButtonItemPaymentMethod.ItemClick += new DevExpress.XtraBars.ItemClickEventHandler(this.barButtonItemPaymentMethod_ItemClick);
+            // 
+            // bar3
+            // 
+            this.bar3.BarName = "Status bar";
+            this.bar3.CanDockStyle = DevExpress.XtraBars.BarCanDockStyle.Bottom;
+            this.bar3.DockCol = 0;
+            this.bar3.DockRow = 0;
+            this.bar3.DockStyle = DevExpress.XtraBars.BarDockStyle.Bottom;
+            this.bar3.OptionsBar.AllowQuickCustomization = false;
+            this.bar3.OptionsBar.DrawDragBorder = false;
+            this.bar3.OptionsBar.UseWholeRow = true;
+            this.bar3.Text = "Status bar";
+            // 
+            // barDockControlTop
+            // 
+            this.barDockControlTop.CausesValidation = false;
+            this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
+            this.barDockControlTop.Size = new System.Drawing.Size(1448, 22);
+            // 
+            // barDockControlBottom
+            // 
+            this.barDockControlBottom.CausesValidation = false;
+            this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 697);
+            this.barDockControlBottom.Size = new System.Drawing.Size(1448, 23);
+            // 
+            // barDockControlLeft
+            // 
+            this.barDockControlLeft.CausesValidation = false;
+            this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 22);
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 675);
+            // 
+            // barDockControlRight
+            // 
+            this.barDockControlRight.CausesValidation = false;
+            this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
+            this.barDockControlRight.Location = new System.Drawing.Point(1448, 22);
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 675);
+            // 
+            // dockManager1
+            // 
+            this.dockManager1.Form = this;
+            this.dockManager1.MenuManager = this.barManager1;
+            this.dockManager1.RootPanels.AddRange(new DevExpress.XtraBars.Docking.DockPanel[] {
+            this.dockPanel1});
+            this.dockManager1.TopZIndexControls.AddRange(new string[] {
+            "DevExpress.XtraBars.BarDockControl",
+            "DevExpress.XtraBars.StandaloneBarDockControl",
+            "System.Windows.Forms.StatusBar",
+            "System.Windows.Forms.MenuStrip",
+            "System.Windows.Forms.StatusStrip",
+            "DevExpress.XtraBars.Ribbon.RibbonStatusBar",
+            "DevExpress.XtraBars.Ribbon.RibbonControl",
+            "DevExpress.XtraBars.Navigation.OfficeNavigationBar",
+            "DevExpress.XtraBars.Navigation.TileNavPane"});
+            // 
+            // dockPanel1
+            // 
+            this.dockPanel1.Controls.Add(this.dockPanel1_Container);
+            this.dockPanel1.Dock = DevExpress.XtraBars.Docking.DockingStyle.Left;
+            this.dockPanel1.ID = new System.Guid("bc46edda-9530-41b6-8d80-35602044dddd");
+            this.dockPanel1.Location = new System.Drawing.Point(0, 22);
+            this.dockPanel1.Name = "dockPanel1";
+            this.dockPanel1.OriginalSize = new System.Drawing.Size(435, 200);
+            this.dockPanel1.Size = new System.Drawing.Size(435, 675);
+            this.dockPanel1.Text = "Danh sách ngân hàng";
+            // 
+            // dockPanel1_Container
+            // 
+            this.dockPanel1_Container.Controls.Add(this.ctrBank1);
+            this.dockPanel1_Container.Location = new System.Drawing.Point(4, 23);
+            this.dockPanel1_Container.Name = "dockPanel1_Container";
+            this.dockPanel1_Container.Size = new System.Drawing.Size(427, 648);
+            this.dockPanel1_Container.TabIndex = 0;
+            // 
+            // ctrBank1
+            // 
+            this.ctrBank1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.ctrBank1.Location = new System.Drawing.Point(0, 0);
+            this.ctrBank1.Name = "ctrBank1";
+            this.ctrBank1.Size = new System.Drawing.Size(427, 648);
+            this.ctrBank1.TabIndex = 0;
+            this.ctrBank1.ExcuteCommand += new WSS.Financial.Bank.ctrBank.ChangedEventHandler(this.ctrBank1_ExcuteCommand);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1448, 720);
+            this.Controls.Add(this.dockPanel1);
+            this.Controls.Add(this.barDockControlLeft);
+            this.Controls.Add(this.barDockControlRight);
+            this.Controls.Add(this.barDockControlBottom);
+            this.Controls.Add(this.barDockControlTop);
+            this.IsMdiContainer = true;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hệ thống trang tài chính websosanh.vn";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.barManager1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dockManager1)).EndInit();
+            this.dockPanel1.ResumeLayout(false);
+            this.dockPanel1_Container.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraBars.BarManager barManager1;
+        private DevExpress.XtraBars.Bar bar2;
+        private DevExpress.XtraBars.Bar bar3;
+        private DevExpress.XtraBars.BarDockControl barDockControlTop;
+        private DevExpress.XtraBars.BarDockControl barDockControlBottom;
+        private DevExpress.XtraBars.BarDockControl barDockControlLeft;
+        private DevExpress.XtraBars.BarDockControl barDockControlRight;
+        private DevExpress.XtraBars.BarSubItem barSubItemVayTinDung;
+        private DevExpress.XtraBars.BarButtonItem barButtonItemBankLending;
+        private DevExpress.XtraBars.BarSubItem barSubItem1;
+        private DevExpress.XtraBars.BarButtonItem barButtonItemBankBlending;
+        private DevExpress.XtraBars.BarSubItem barSubItem2;
+        private DevExpress.XtraBars.BarButtonItem barButtonItemPaymentMethod;
+        private DevExpress.XtraBars.Docking.DockManager dockManager1;
+        private DevExpress.XtraBars.Docking.DockPanel dockPanel1;
+        private DevExpress.XtraBars.Docking.ControlContainer dockPanel1_Container;
+        private Bank.ctrBank ctrBank1;
+    }
+}
+
