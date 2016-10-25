f548d816c14fb144b4bfe2e6d0d38d64e8009a73
xuantrangk54 <xuantrangk54@gmail.com>
2016-10-25 13:49:11 +0700

    Masoffer: Auto change link for company masoffer

--- WSS.DocMan/WorkerCrawler.cs
+++ WSS.DocMan/WorkerCrawler.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using GABIZ.Base.HtmlAgilityPack;
@@ -14,7 +15,7 @@ namespace WSS.DocMan
 {
     public class WorkerCrawler : QueueingConsumer
     {
-        readonly DocManAdapter _docManAdapter = new DocManAdapter();
+        private readonly DocManAdapter _docManAdapter = new DocManAdapter();
         private ILog _log = log4net.LogManager.GetLogger(typeof (WorkerCrawler));
 
         public WorkerCrawler() : base(
@@ -24,7 +25,7 @@ namespace WSS.DocMan
 
         public override void Init()
         {
-           
+
         }
 
         public override void ProcessMessage(BasicDeliverEventArgs message)
@@ -32,32 +33,37 @@ namespace WSS.DocMan
             JobQueue job =
                 Newtonsoft.Json.JsonConvert.DeserializeObject<JobQueue>(UTF8Encoding.UTF8.GetString(message.Body));
             string url = job.Url;
-         
-            var html  = GABIZ.Base.HtmlUrl.HTMLTransmitter.getHTML(url, 42, 2);
-            if (!string.IsNullOrEmpty(html))
+
+            if (Regex.IsMatch(job.Url, @"http://moj.gov.vn/vbpq/Lists/Vn%20bn%20php%20lut/View_Detail.aspx\?ItemID=\d.*"))
             {
-                html = System.Web.HttpUtility.HtmlDecode(html);
-                HtmlDocument htmlDocument = new HtmlDocument();
-                htmlDocument.LoadHtml(html);
-                Documet document = new Documet();
-
-                ParserData p = new ParserData();
-                p.Parse(ref document, htmlDocument, url);
-                if (document.IsValidData())
+            var html = GABIZ.Base.HtmlUrl.HTMLTransmitter.getHTML(url, 42, 2);
+                if (!string.IsNullOrEmpty(html))
                 {
-                    _docManAdapter.InsertData(document);
+                    html = System.Web.HttpUtility.HtmlDecode(html);
+                    HtmlDocument htmlDocument = new HtmlDocument();
+                    htmlDocument.LoadHtml(html);
+                    Documet document = new Documet();
+
+                    ParserData p = new ParserData();
+                    bool bOK = p.Parse(ref document, htmlDocument, url);
+                    if (bOK && document.IsValidData())
+                    {
+                        _docManAdapter.InsertData(document);
+                    }
                 }
+                _log.Info(string.Format("{0} Success", url));
+
+                Thread.Sleep(1000);
+
+                //string urlInfo = @"http://moj.gov.vn/vbpq/Pages/View_Propertes.aspx?ItemID=3001";
+                //HtmlDocument htmlDocumentInfo = new HtmlDocument();
+                //htmlDocumentInfo.LoadHtml(GABIZ.Base.HtmlUrl.HTMLTransmitter.getHTML(url, 42, 2));
             }
-            _log.Info(string.Format("{0} Success", url));
+            else
+            {
+                Logger.Info(string.Format("Fail regex {0}", job.Url));
+            }
             this.GetChannel().BasicAck(message.DeliveryTag, true);
-            Thread.Sleep(1000);
-
-            //string urlInfo = @"http://moj.gov.vn/vbpq/Pages/View_Propertes.aspx?ItemID=3001";
-            //HtmlDocument htmlDocumentInfo = new HtmlDocument();
-            //htmlDocumentInfo.LoadHtml(GABIZ.Base.HtmlUrl.HTMLTransmitter.getHTML(url, 42, 2));
-
-
-
         }
     }
 }
--- WebsosanhCacheTool/WebMerchantProductCacheTool.cs
+++ WebsosanhCacheTool/WebMerchantProductCacheTool.cs
@@ -12,12 +12,14 @@ using UTF_Converter;
 using WebsosanhCacheTool.ProductDataSetTableAdapters;
 using Websosanh.Core.Drivers.Redis;
 using ProtoBuf;
+using QT.Entities.MasOffer;
 
 namespace WebsosanhCacheTool
 {
     public class WebMerchantProductCacheTool
     {
         private static readonly ILog Log = LogManager.GetLogger(typeof (WebMerchantProductCacheTool));
+        private static MasOfferAdapter masOffer = MasOfferAdapter.Instance();
 
         public static void InsertAllWebMerchantProductToCache(string connProductString)
         {
@@ -48,16 +50,24 @@ namespace WebsosanhCacheTool
             {
                 Log.Error("InsertAllWebMerchantProductToCache Error", ex);
             }
-
         }
 
         public static void InsertWebMerchantProductToCache(long companyID, string companyDomain, string connProductString)
         {
             try
             {
-                    //Get List Product Of Company
+                //Get List Product Of Company
                 var listWebMerchantProducts = WebMerchantProductBAL.GetListWebMerchantProductsOfCompany(companyID,
                         connProductString);
+
+                if (masOffer.CheckIsMasOffer(companyID))
+                {
+                    foreach (var itemProduct in listWebMerchantProducts)
+                    {
+                        itemProduct.DetailUrl = masOffer.GetFullUrl(companyID, itemProduct.DetailUrl);
+                    }
+                }
+
                 if (listWebMerchantProducts.Count > 0)
                     {
                         var parts = CollectionUtilities.Partition<WebMerchantProduct>(listWebMerchantProducts, 10000);
@@ -73,7 +83,6 @@ namespace WebsosanhCacheTool
             {
                 Log.Error("InsertWebMerchantProductToCache Error", ex);
             }
-
         }
 
     }
