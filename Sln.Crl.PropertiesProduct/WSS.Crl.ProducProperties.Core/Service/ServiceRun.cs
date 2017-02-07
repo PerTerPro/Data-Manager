9ff2c7998903df073d2f662930e1950cec1164c1
manhdo297 <manh29793@gmail.com>
2017-02-07 13:23:25 +0700

    MerchantFacebook

--- Sln.Crl.PropertiesProduct/WSS.Crl.ProducProperties.Core/Service/ServiceRun.cs
+++ Sln.Crl.PropertiesProduct/WSS.Crl.ProducProperties.Core/Service/ServiceRun.cs
@@ -12,12 +12,13 @@ using WSS.Crl.ProducProperties.Core.Entity;
 using WSS.Crl.ProducProperties.Core.Storage;
 using WSS.Crl.ProducProperties.Core.Worker;
 using WSS.LibExtra;
+using System.Web;
 
 namespace WSS.Crl.ProducProperties.Core.Service
 {
     public static class ServiceRun
     {
-        private static ILog log = LogManager.GetLogger(typeof (ServiceRun));
+        private static ILog log = LogManager.GetLogger(typeof(ServiceRun));
         public static void PushJobDownload(IEnumerable<string> domains)
         {
             foreach (var domain in domains)
@@ -37,16 +38,34 @@ namespace WSS.Crl.ProducProperties.Core.Service
 
         private static void PushJobDownload(string domain)
         {
+
+
             ProducerBasic producer = new ProducerBasic(RabbitMQManager.GetRabbitMQServer(ConfigStatic.KeyRabbitMqCrlProductProperties),
                 ConfigStatic.GetQueueWaitDownloadHtml(domain));
             IStorageProduct storageProduct = new StorageProduct();
             int i = 0;
+            string DetailUrl = "";
+
             storageProduct.ProcessProduct(domain, (sender, product) =>
             {
+                if (domain == "lazada.vn")
+                {
+                    string urlencode = product.DetailUrl;
+                    string urldecode = HttpUtility.UrlDecode(product.DetailUrl);
+                    char charRange = '?';
+                    int startIndex = urldecode.IndexOf(charRange)+1;
+                    int endIndex = urldecode.LastIndexOf(charRange)-1;
+                    int length = endIndex - startIndex + 1;
+                    DetailUrl = urldecode.Substring(startIndex, length).Replace("url=", "");
+                }
+                else
+                {
+                    DetailUrl = product.DetailUrl;
+                }
                 producer.PublishString(new JobCrlProperties()
                 {
                     ProductId = product.Id,
-                    DetailUrl = UtilCrl.GetUrl(product.DetailUrl,domain),
+                    DetailUrl = UtilCrl.GetUrl(DetailUrl, domain),
                     Domain = domain,
                     ClassificationId = product.ClassificationId,
                     Classification = product.Classification
