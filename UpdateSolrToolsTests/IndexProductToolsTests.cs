45f18b49a35aa596b83fff57aefa5fa70652e35d
hoangvu88 <hoangv.hust@gmail.com>
2017-02-24 14:27:23 +0700

    add Test

--- UpdateSolrToolsTests/IndexProductToolsTests.cs
+++ UpdateSolrToolsTests/IndexProductToolsTests.cs
@@ -10,6 +10,7 @@ namespace UpdateSolrTools.Tests
     [TestFixture()]
     public class IndexProductToolsTests
     {
+        const string productConnectionString = "Data Source=42.112.28.93;Initial Catalog=QT_2;Persist Security Info=True;User ID=wss_price";
         [Test()]
         public void GetUnitNormalizedNameTest()
         {
@@ -29,6 +30,34 @@ namespace UpdateSolrTools.Tests
                 UnitNormalization.GetUnitNormalizedName("Dép hàng không Vietnam Airlines mẫu mới màu vàng mới 100m tuy");
             var otherNames8 =
                 UnitNormalization.GetUnitNormalizedName("Dép hàng không Vietnam Airlines mẫu mới màu vàng mới 100 m tuy");
+            return;
+        }
+
+        [Test()]
+        public void GetAllPriorMerchantsTest()
+        {
+            var priorMerchant = IndexProductTools.GetAllPriorMerchants(productConnectionString);
+            return;
+        }
+
+        [Test()]
+        public void GetAllBadMerchantIdTest()
+        {
+            var priorMerchant = IndexProductTools.GetAllBadMerchantId(productConnectionString, 45);
+            return;
+        }
+
+        [Test()]
+        public void GetListMerchantUseDatafeedIDTest()
+        {
+            var priorMerchant = IndexProductTools.GetListMerchantUseDatafeedID(productConnectionString);
+            return;
+        }
+
+        [Test()]
+        public void GetListSpecialMerchantIDTest()
+        {
+            var priorMerchant = IndexProductTools.GetListSpecialMerchantID(productConnectionString);
             return;
         }
     }
