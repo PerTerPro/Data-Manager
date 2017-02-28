d59267f9b69d34659260c9c48da343e763aa7c6e
manhdo297 <manh29793@gmail.com>
2017-02-28 15:08:32 +0700

    GetSizeImg

---
+++ WSS.Image.GetSize/Object/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSS.Image.GetSize.Object
+{
+    public class Company
+    {
+        public long ID { get; set; }
+        public int TimeDelay { get; set; }
+    }
+}
---
+++ WSS.Service.ReportProductAdsScoreError/Object/ProductAdsScore.cs
@@ -0,0 +1,48 @@
+﻿using ProtoBuf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WSS.Service.ReportProductAdsScoreError.Object
+{
+    [ProtoContract]
+    public class ProductAdsScore
+    {
+        [ProtoMember(1)]
+        public long ProductId { get; set; }
+        [ProtoMember(2)]
+        public long CompanyId { get; set; }
+        [ProtoMember(4)]
+        public DateTime StartDate { get; set; }
+        [ProtoMember(5)]
+        public DateTime EndDate { get; set; }
+        [ProtoMember(6)]
+        public string Keyword { get; set; }
+        [ProtoMember(7)]
+        public DateTime DateCreate { get; set; }
+        [ProtoMember(8)]
+        public DateTime LastUpdate { get; set; }
+        [ProtoMember(9)]
+        public int Order { get; set; }
+        [ProtoMember(10)]
+        public int OrderAdsScore { get; set; }
+        [ProtoMember(11)]
+        public int KeywordAdsId { get; set; }
+        [ProtoMember(12)]
+        public string Domain { get; set; }
+        [ProtoMember(13)]
+        public string DetailUrl { get; set; }
+
+        public string GetJson()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
+
+        public static ProductAdsScore FromJson(string str)
+        {
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<ProductAdsScore>(str);
+        }
+    }
+}
